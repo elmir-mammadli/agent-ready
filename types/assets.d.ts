5d2ab8b3391b4c10b1f63332b220e79b95119599
Elmir's Mac mini <elmirmammadli18@gmail.com>
2026-05-17 14:30:27 -0400

    chore: move landing page to separate private repo

--- types/assets.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.svg?url' {
-  const src: string
-  export default src
-}
-
-declare module '*.svg?url&no-inline' {
-  const src: string
-  export default src
-}
--- types/shiki.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '#app' {
-  interface NuxtApp {
-    $codeToHtml: (code: string, language?: string) => Promise<string>
-  }
-}
-
-export {}
